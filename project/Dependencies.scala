ce2f6c8f2d2cfc64e84321e55662a52b4cfa6d3b
Scala Steward <me@scala-steward.org>
2020-02-15 14:11:32 +0100

    Update circe-core, circe-literal, ... to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,9 @@ object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.1.0"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.1.1"
   lazy val catsMtl = "org.typelevel" %% "cats-mtl-core" % "0.7.0"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.12.3"
-  lazy val circeLiteral = "io.circe" %% "circe-literal" % "0.12.3" // used for test only
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.12.3" // used for site only
+  lazy val circeCore = "io.circe" %% "circe-core" % "0.13.0"
+  lazy val circeLiteral = "io.circe" %% "circe-literal" % "0.13.0" // used for test only
+  lazy val circeParser = "io.circe" %% "circe-parser" % "0.13.0" // used for site only
   lazy val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.3.17"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   lazy val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "6.3"
