20337b931d52d8fbf819a53cb381dbc8014fa13c
Scala Steward <me@scala-steward.org>
2020-04-06 00:06:32 +0200

    Update diffx-scalatest to 0.3.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "io.circe" %% "circe-literal" % "0.13.0" // used for test only
   lazy val circeParser =
     "io.circe" %% "circe-parser" % "0.13.0" // used for site only
-  lazy val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.3.20"
+  lazy val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.3.23"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   lazy val logstash =
     "net.logstash.logback" % "logstash-logback-encoder" % "6.3"
