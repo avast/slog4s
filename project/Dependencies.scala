3f73a8d3af1a2f5d3c0815be7f39d8e028fb87af
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-21 09:17:50 +0100

    chore: Update scala-collection-compat to 2.3.0 (#161)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     ))
   lazy val monixDependency = "io.monix" %% "monix" % "3.2.2"
   lazy val scalaCollectionCompat =
-    "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.6"
+    "org.scala-lang.modules" %% "scala-collection-compat" % "2.3.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.1"
   lazy val slf4jDepedency = "org.slf4j" % "slf4j-api" % "1.7.30"
   lazy val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.2.1"
