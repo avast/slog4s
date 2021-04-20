30352a78e07e82dbae49d2190e4768ac918043df
Scala Steward <me@scala-steward.org>
2021-04-21 00:24:20 +0200

    Update zio-interop-cats to 2.4.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,6 +27,6 @@ object Dependencies {
   lazy val slf4jDepedency = "org.slf4j" % "slf4j-api" % "1.7.30"
   lazy val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.2.1"
   lazy val zioDependency = "dev.zio" %% "zio" % "1.0.3"
-  lazy val zioInterop = "dev.zio" %% "zio-interop-cats" % "2.2.0.1"
+  lazy val zioInterop = "dev.zio" %% "zio-interop-cats" % "2.4.1.0"
 
 }
