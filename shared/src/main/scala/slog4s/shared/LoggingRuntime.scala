662149f52397fc113f944df59191d28c44c7294b
Scala Steward <me@scala-steward.org>
2021-10-21 08:08:39 +0200

    Reformat with scalafmt 3.0.7

--- shared/src/main/scala/slog4s/shared/LoggingRuntime.scala
+++ shared/src/main/scala/slog4s/shared/LoggingRuntime.scala
@@ -2,7 +2,8 @@ package slog4s.shared
 
 import slog4s.{LoggerFactory, LoggingContext}
 
-/** Helper trait that is used by factories to build both [[LoggerFactory]] and [[LoggingContext]].
+/** Helper trait that is used by factories to build both [[LoggerFactory]] and
+  * [[LoggingContext]].
   */
 trait LoggingRuntime[F[_]] {
   def loggerFactory: LoggerFactory[F]
