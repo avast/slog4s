098bd77f414507ebd11a0b5800dbbcaaf56fcc2f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-21 21:29:44 +0100

    Update logback-classic to 1.2.11 (#288)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val circeParser =
     "io.circe" %% "circe-parser" % "0.14.3" // used for site only
   lazy val diffx = "com.softwaremill.diffx" %% "diffx-scalatest" % "0.3.30"
-  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.11"
   lazy val logstash =
     "net.logstash.logback" % "logstash-logback-encoder" % "6.6"
   lazy val kindProjector =
