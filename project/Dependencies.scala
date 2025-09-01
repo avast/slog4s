0f7585894e8cd2f14dc7efc340e3f2f595eea4b6
Scala Steward <scala_steward@virtuslab.com>
2025-09-01 13:18:46 +0000

    Update sourcecode to 0.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     "org.scala-lang.modules" %% "scala-collection-compat" % "2.12.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   lazy val slf4jDepedency = "org.slf4j" % "slf4j-api" % "2.0.13"
-  lazy val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.4.2"
+  lazy val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.4.4"
   lazy val zioDependency = "dev.zio" %% "zio" % "1.0.18"
   lazy val zioInterop = "dev.zio" %% "zio-interop-cats" % "2.2.0.1"
 
